04f64ac404a7877f58f5993006cc82904d7a6a7f
Piotrek Bulik <bulik.piotr@gmail.com>
2017-04-13 20:22:49 +0200

    prepared to run on server

--- football_manager/app_football/management/commands/task.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-import kronos
-
-
-@kronos.register('0 0 * * *')
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        print('Hello, world!')
